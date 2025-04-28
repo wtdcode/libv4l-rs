23eef5367a60d08927f80da6e62fba3f63f5b88d
mio <mio@lazym.io>
2025-04-28 18:03:59 +0800

    sysroot from cc

--- v4l2-sys/build.rs
+++ v4l2-sys/build.rs
@@ -3,6 +3,21 @@ extern crate bindgen;
 use std::env;
 use std::path::{Path, PathBuf};
 
+fn print_cc(cc: &Path, args: &[&str]) -> String {
+    let out = std::process::Command::new(cc.to_str().expect("utf-8?"))
+        .args(args)
+        .output()
+        .expect("fail to run cc");
+    if !out.status.success() {
+        panic!(
+            "sysroot not found with {} with {:?}",
+            String::from_utf8_lossy(&out.stderr),
+            args
+        );
+    }
+    String::from_utf8(out.stdout).expect("non utf-8?!")
+}
+
 fn main() {
     let extra_include_paths = if cfg!(target_os = "freebsd") {
         assert!(
@@ -11,14 +26,25 @@ fn main() {
             against `libv4l2` and the header file is missing.\n\
             Consider installing `multimedia/v4l_compat` FreeBSD package."
         );
-        vec!["-I/usr/local/include"]
+        vec!["-I/usr/local/include".to_string()]
     } else {
         vec![]
     };
 
+    let cc = cc::Build::new().get_compiler();
+
+    let cc_sysroot = print_cc(cc.path(), &["--print-sysroot"]).trim().to_string();
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_args(extra_include_paths)
+        .clang_args(
+            extra_include_paths.into_iter().chain(
+                vec![
+                    format!("--sysroot={}", &cc_sysroot),
+                    format!("-I{}/usr/include/linux", cc_sysroot),
+                ]
+                .into_iter(),
+            ),
+        )
         .generate()
         .expect("Failed to generate bindings");
 
